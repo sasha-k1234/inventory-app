271d581fb2c321e976ec53ffed6f89179173501b
sasha-1234@ <ja4349950@gmail.com>
2024-03-26 22:10:53 +0100

    productinstance create added

--- project/controllers/productinstanceController.js
+++ project/controllers/productinstanceController.js
@@ -28,13 +28,52 @@ exports.productinstance_list = asyncHandler(async (req, res, next) => {
   
   // Display ProductInstance create form on GET.
   exports.productinstance_create_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: ProductInstance create GET");
+    const allProducts = await Product.find({},"title").sort({title:1}).exec();
+
+    res.render("productinstance_form",{
+      title:"Create ProductInstance",
+      products_list:allProducts,
+    });
   });
   
   // Handle ProductInstance create on POST.
-  exports.productinstance_create_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: ProductInstance create POST");
-  });
+  exports.productinstance_create_post = [
+    body("product","Product must be specified")
+        .trim()
+        .isLength({min:3})
+        .escape(),
+    body("status").escape(),
+    body("in_stock","Invalid Date")
+        .optional({values:"falsy"})
+        .isISO8601()
+        .toDate(),
+
+    asyncHandler(async(req,res,next)=>{
+      const errors = validationResult(req);
+
+      const productInstance = new ProductInstance({
+        product:req.body.product,
+        status:req.body.status,
+        in_stock:req.body.in_stock,
+      });
+
+      if (!errors.isEmpty()) {
+        const allProducts = await Product.find({},"title").sort({title:1}).exec();
+
+        res.render("productinstance_form",{
+          title:"Create ProductInstance",
+          product_list:allProducts,
+          selected_product:productInstance.product._id,
+          errors:errors.array(),
+          productInstance:productInstance,
+        });
+        return;
+      } else {
+        await productInstance.save();
+        res.redirect(productInstance.url);
+      }
+    }),
+  ];
   
   // Display ProductInstance delete form on GET.
   exports.productinstance_delete_get = asyncHandler(async (req, res, next) => {
